9cd57db773c11cc114d9dd7559ad3791c203d720
Anubhav Gupta <mail.anubhav06@gmail.com>
2022-08-13 23:47:31 +0530

    removed user authentication
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- backend/portal/api/urls.py
+++ backend/portal/api/urls.py
@@ -1,13 +1,5 @@
 from django.urls import path
 from . import views
-from .views import MyTokenObtainPairView
-
-# Refer to: https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html#installation 
-# for installation of JWT with DRF
-
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 
 # Refer to the corresponding view function for more detials of the url routes
 urlpatterns = [ 
@@ -16,9 +8,5 @@ urlpatterns = [
     
     path('add-tweet/', views.add_tweet, name='addTweet'),
     path('get-tweet/', views.get_tweet, name='getTweet'),
-
-    # For user authentication
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
--- backend/portal/api/views.py
+++ backend/portal/api/views.py
@@ -1,42 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
-
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.tokens import RefreshToken
-
-import re
 
 from portal.models import Tweet
 from portal.api.serializers import TweetSerializer
 
-from backend.settings import nlp
+import re
 import requests
 
-
-# For customizing the token claims: (whatever value we want)
-# Refer here for more details: https://django-rest-framework-simplejwt.readthedocs.io/en/latest/customizing_token_claims.html
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-
-        # Add custom claims
-        token['username'] = user.username
-        
-        if user.groups.filter(name="Restaurant").exists():
-            token['group'] = "Restaurant"
-        else:
-            token['group'] = "None"
-        # ...
-
-        return token
-
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+from backend.settings import nlp
 
 
 # THE_ALGORITHM to validate the tweet
